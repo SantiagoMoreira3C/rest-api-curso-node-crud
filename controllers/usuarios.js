ef767ffcbe7c6ddec967ba6ef8cbca8f8dbd4bf0
Santiago Moreira Palma <sanmopa2000@gmail.com>
2022-09-10 10:20:59 -0500

    rest server crud user

---
+++ controllers/usuarios.js
@@ -0,0 +1,89 @@
+const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
+
+
+
+const usuariosGet = async(req = request, res = response) => {
+    
+    // const query = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true};
+  
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ])
+    
+    res.json({  
+        total,
+        usuarios
+    });
+}
+const usuariosPost = async (req, res = response) => {
+   
+
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
+    //Verificar si el correo existe
+ 
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt);
+
+    //Guardar en DB
+
+    await usuario.save();
+    res.json({
+        usuario
+    });
+}
+const usuariosPut = async (req, res = response) => {
+
+    const {id} = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+    
+    //TODO validar contra base de datos
+    if(password){
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt);
+    }
+    
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+    res.json({
+        msg: 'put API - controlador',
+        usuario
+    });
+}
+const usuariosPatch = (req, res = response) => {
+    const {id} = req.params;
+    res.json({
+       id
+    });
+}
+const usuariosDelete = async(req, res = response) => {
+    const {id} = req.params;
+    //fisicamente lo borramos
+
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
+
+    res.json({
+        usuario
+    });
+}
+
+
+
+module.exports={
+     usuariosGet,
+     usuariosPost,
+     usuariosPut,
+     usuariosPatch,
+     usuariosDelete
+} 
+
---
+++ helpers/db-validators.js
@@ -0,0 +1,32 @@
+const Role = require('../models/roles');
+const Usuario = require('../models/usuario');
+
+const esRolValido =   async(rol='')=>{
+    const existeRol = await Role.findOne({ rol }) 
+    if (!existeRol){
+         throw new Error(`El rol ${rol} no está registrado en la BD`)
+    }
+}
+
+const emailExiste = async(correo='')=> {
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail){
+        throw new Error(`El correo ${correo} ya está registrado en la BD`)
+    }
+}
+
+const existeUsuarioPorId = async(id='')=> {
+    const existeUsuario = await Usuario.findById(id);
+    if (!existeUsuario){
+        throw new Error(`El id ${id} no existe`)
+    }
+}
+
+
+
+
+module.exports= {
+    esRolValido,
+    emailExiste,
+    existeUsuarioPorId
+}
